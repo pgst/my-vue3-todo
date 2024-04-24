bac241293c26da7027571a731fc9dbc56ed5b1a5
Shinichi TAKATSUKA <pgstaka@gmail.com>
2024-04-24 17:45:19 +0900

    TODOの一覧表示

---
+++ my-vue3-todo/src/App.vue
@@ -0,0 +1,34 @@
+<!-- ロジック（JavaScriptを書くところ） -->
+<script setup>
+import MainTodo from '@/components/MainTodo.vue'
+import TheFooter from '@/components/TheFooter.vue'
+import TheHeader from '@/components/TheHeader.vue'
+</script>
+
+<!-- テンプレート（HTMLを書くところ） -->
+<template>
+  <div class="wrap">
+    <TheHeader>ヘッダー</TheHeader>
+    <main class="main"><MainTodo /></main>
+    <TheFooter>フッター</TheFooter>
+  </div>
+</template>
+
+<!-- スタイル（CSSを書くところ） -->
+<style scoped>
+.wrap {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 90vh;
+  padding: 0;
+  margin: 0;
+  font-family: Arial, sans-serif;
+  background-color: #f8f8f8;
+}
+
+.main {
+  flex: 1;
+  padding: 16px;
+}
+</style>
---
+++ my-vue3-todo/src/components/TheHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1 class="title">TODO</h1>
+</template>
+
+<style scoped>
+.title {
+  width: 100%;
+  padding: 16px 0;
+  margin: 0;
+  padding: 16px;
+  text-align: center;
+  background-color: #fff;
+}
+</style>
