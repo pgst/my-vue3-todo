e2306a2bfc451edc7b86e7fa6214c88668a3e4a8
Shinichi TAKATSUKA <pgstaka@gmail.com>
2024-04-25 14:38:44 +0900

    ルートを設定

--- my-vue3-todo/src/router/index.js
+++ my-vue3-todo/src/router/index.js
@@ -1,8 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import MainTodo from '@/views/MainTodo.vue'
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: []
+  routes: [
+    {
+      path: '/',
+      name: 'top',
+      component: MainTodo
+    },
+    {
+      path: '/mainTodo',
+      name: 'mainTodo',
+      component: MainTodo
+    }
+  ]
 })
 
 export default router
