68649d272ce1326e5b2a71a141d381ba79a9fbd8
Shinichi TAKATSUKA <pgstaka@gmail.com>
2024-04-25 11:12:17 +0900

    コンポーネント間のやりとり（親から子へ）

---
+++ my-vue3-todo/src/components/BaseButton.vue
@@ -0,0 +1,32 @@
+<script setup>
+const props = defineProps({ color: String })
+
+const aaa = () => {}
+</script>
+
+<template>
+  <button class="btn" :class="props.color" @click="aaa"><slot /></button>
+</template>
+
+<style scoped>
+.btn {
+  padding: 8px;
+  font-size: 14px;
+  color: #fff;
+  text-align: center;
+  /* background-color: #03a9f4; */
+  border-radius: 6px;
+}
+
+.bule {
+  background-color: #03a9f4;
+}
+
+.green {
+  background-color: #00c853;
+}
+
+.pink {
+  background-color: #ff4081;
+}
+</style>
--- my-vue3-todo/src/components/MainTodo.vue
+++ my-vue3-todo/src/components/MainTodo.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref } from 'vue'
 import { useTodoList } from '@/composables/useTodoList'
+import BaseButton from '@/components/BaseButton.vue'
 
 const todo = ref('')
 const isEdit = ref(false)
@@ -41,6 +42,11 @@ const changeCheck = (id) => {
 </script>
 
 <template>
+  <BaseButton color="bule">追加</BaseButton>
+  <BaseButton color="green">編</BaseButton>
+  <BaseButton color="pink">削</BaseButton>
+  <BaseButton color="green">変更</BaseButton>
+
   <div>
     <input type="text" class="todo_input" v-model="todo" placeholder="+ TODOを入力" />
     <button class="btn green" @click="editTodo" v-if="isEdit">変更</button>
