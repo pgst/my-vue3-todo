9b5614828f1903b091649c9352004b6fe6b1f3c8
Shinichi TAKATSUKA <pgstaka@gmail.com>
2024-04-25 16:43:14 +0900

    リアクティブの監視

--- my-vue3-todo/src/views/BlogPage.vue
+++ my-vue3-todo/src/views/BlogPage.vue
@@ -1,9 +1,16 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import { useRoute } from 'vue-router'
 
 const route = useRoute()
 const id = ref(route.params.id)
+
+console.log('watch外', id.value)
+
+watch(route, () => {
+  id.value = route.params.id
+  console.log('watch内', id.value)
+})
 </script>
 
 <template>
